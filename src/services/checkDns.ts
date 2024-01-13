406c0d4664e988568fe8bfcfb800156fae077126
Kristof Siket <kristofsiket@gmail.com>
2024-01-13 21:20:52 +0100

    feat(core): some clarification on the output

--- src/services/checkDns.ts
+++ src/services/checkDns.ts
@@ -1,15 +1,37 @@
-import dns from "dns";
+import dns, { RecordWithTtl } from "dns";
 
 type CheckDnsProps = {
   domain: string;
 };
 
 const checkDns = async ({ domain }: CheckDnsProps) => {
-  const dnsPromises = dns.promises;
-  const result = await dnsPromises.resolve(domain);
+  try {
+    const dnsPromises = dns.promises;
+    const result: RecordWithTtl[] = await dnsPromises.resolve4(domain, {
+      ttl: true,
+    });
+    dnsPromises.resolve4;
 
-  console.log(`Resolved IPs for ${domain}: [${result}]`);
-  return result;
+    console.log(
+      `Resolved IPs for ${domain}: [${result.map((r) => r.address).join(", ")}]`
+    );
+    return {
+      resolved: true,
+      result,
+    };
+  } catch (err) {
+    console.error(err);
+
+    if (err.code === "ENOTFOUND") {
+      console.log(`Domain ${domain} could not be resolved!`);
+      return {
+        resolved: false,
+        result: [],
+      };
+    }
+
+    throw err;
+  }
 };
 
 export default checkDns;
