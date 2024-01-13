49d12e0ef8fe58049f0ea6c73c3b2f914ae9bd91
Kristof Siket <kristofsiket@gmail.com>
2024-01-13 20:52:17 +0100

    chore: add logging for operation duration

--- src/utils/speed.ts
+++ src/utils/speed.ts
@@ -4,6 +4,8 @@ const speed = async <T>(op: Promise<T>): Promise<T & { latency: number }> => {
   const start = performance.now();
   const result = await op;
   const end = performance.now();
+
+  console.log(`Operation took ${end - start}ms`);
   return { ...result, latency: end - start };
 };
 
